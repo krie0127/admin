aef21c35a2a032f5e3fb9019d18df23eb05866e0
your name <your-email@example.com>
2023-09-09 08:53:36 +0900

    ルーティン設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "posts#index"
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
@@ -8,4 +9,12 @@ Rails.application.routes.draw do
   end
   resources :users, only: %i[new create]
   resource :profile, only: %i[show edit update]
+  namespace :admin do
+    root to: "dashboards#index"
+    get 'login', to: 'user_sessions#new'
+    post 'login', to: 'user_sessions#create'
+    delete 'logout', to: 'user_sessions#destroy'
+    resources :posts, only: %i[index edit update show destroy]
+    resources :users, only: %i[index edit update show destroy]
+  end
 end
