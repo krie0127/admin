5b841b71fcf7d2a5e5d2a090ada0eda211975feb
your name <your-email@example.com>
2023-09-09 08:46:27 +0900

    管理者のログイン機能を担うコントローラを作成

---
+++ app/controllers/admin/user_sessions_controller.rb
@@ -0,0 +1,22 @@
+class Admin::UserSessionsController < Admin::BaseController
+    skip_before_action :require_login, only: %i[new create]
+    skip_before_action :check_admin, only: %i[new create]
+    layout 'layouts/admin_login'
+  
+    def new; end
+  
+    def create
+      @user = login(params[:email], params[:password])
+      if @user
+        redirect_to admin_root_path
+      else
+        flash.now[:danger] = "Login failed"
+      end
+    end
+  
+    def destroy
+      logout
+      flash[:success] = ' successful'
+      redirect_to admin_login_path
+    end
+  end
