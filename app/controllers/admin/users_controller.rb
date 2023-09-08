8de0b56916b4808554e27141de22663a961a3d7e
your name <your-email@example.com>
2023-09-09 08:50:51 +0900

    ユーザーコントローラ作成

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,36 @@
+class Admin::UsersController < Admin::BaseController
+    before_action :set_user, only: %i[edit update show destroy]
+  
+    def index
+      @users = User.order(created_at: :desc)
+    end
+  
+    def edit; end
+  
+    def update
+      if @user.update(user_params)
+          redirect_to admin_user_path(@user)
+      else
+        flash.now[:danger]
+        render :edit
+      end
+    end
+  
+    def show; end
+  
+    def destroy
+      @user.destroy!
+      redirect_to admin_users_path
+    end
+  
+    private
+  
+    def set_user
+      @user = User.find(params[:id])
+    end
+  
+    def user_params
+      params.require(:user).permit(:email, :first_name, :last_name, :avatar, :avatar_cache, :role)
+    end
+  end
+  
