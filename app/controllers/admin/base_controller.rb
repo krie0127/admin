999406b114cdc833c7b2f355862410b9d6771fa4
your name <your-email@example.com>
2023-09-09 21:57:34 +0900

    パスの見直し

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,15 +1,15 @@
 class Admin::BaseController < ApplicationController
-    before_action :check_admin
-    layout 'admin/layouts/application'
+  before_action :check_admin
+  layout 'admin/layouts/application'
   
-    private
+  private
   
-    def not_authenticated
-      flash[:warning] = t('defaults.message.require_login')
-    end
-  
-    def check_admin
-      redirect_to root_path, warning: t('defaults.message.not_autheorized') unless current_user&.admin?
-    end 
+  def not_authenticated
+    flash[:warning] = t('defaults.message.require_login')
   end
+  
+  def check_admin
+    redirect_to admin_login_path, warning: t('defaults.message.not_autheorized') unless current_user&.admin?
+  end 
+end
   
